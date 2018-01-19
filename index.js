f5f797f55656caf47573f44285f03c182ae4981e
John Doe <johndoe@example.com>
2018-01-19 14:39:46 +0100

    change: site http://nu.nl

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const local = require("./pup-local");
     headless: false
   });
   const page = await browser.newPage();
-  await page.goto('https://www.google.com', {
+  await page.goto('https://www.nu.nl', {
     waitUntil: 'networkidle2'
   });
   console.log("page loaded");
 
   // SAVE localStorage
+  // setTimeout(function() {
   await local.saveLocalStorage(page, `${__dirname}/local.json`);
+  // }, 500);
+
   // LOAD localStorage
   // await local.restoreLocalStorage(page, `${__dirname}/local.json`);
 })();
